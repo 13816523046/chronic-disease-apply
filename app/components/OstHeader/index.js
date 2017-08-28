d923a59d24bc2fe1893244caf5251e8d0854df0c
shangboyang <yang8701@gmail.com>
2017-08-28 14:19:51 +0800

    add home

--- app/components/OstHeader/index.js
+++ app/components/OstHeader/index.js
@@ -10,18 +10,18 @@ const OstHeader = ({title, back, option}) => {
       history.back()
   }
 
-  return (
-
-    <div className="ost-header">
-      <div className="ost-header-left" onClick={back || backHandler}></div>
+  return <div className="ost-header">
+      <div className="ost-header-left" onClick={back || backHandler}>
+        <i></i>
+      </div>
       <div className="ost-header-center">
           {title}
       </div>
-      <div>
-          <p className="ost-header-right" onClick={option && option.handler ? option.handler : () => {}}>{ option && option.text ? option.text : '' }</p>
+      <div className="ost-header-right">
+          <p onClick={option && option.handler ? option.handler : () => {}}>{ option && option.text ? option.text : '' }</p>
       </div>
     </div>
-  )
+
 
 }
 
--- app/containers/Home/index.js
+++ app/containers/Home/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { Connect } from './connect'
 import OstHeader from '../../components/OstHeader'
+import './style.less'
+import { Button, Picker, List, TextareaItem, InputItem } from 'antd-mobile'
 
 class Home extends Component {
 
@@ -15,13 +17,40 @@ class Home extends Component {
     }
   }
 
+  state = {
+    data: [{
+      label: '请选择',
+      value: '',
+    },{
+      label: 'key1',
+      value: 'val1',
+    },{
+      label: 'key2',
+      value: 'val2',
+    }],
+    cols: 1, // 1列
+    pickerValue: [],
+    asyncValue: [],
+  }
+
   constructor(props) {
     super(props)
+
+    this.onPickerChange = this.onPickerChange.bind(this)
+  }
+
+  onPickerChange(val) {
+    console.log(val);
+    this.setState({
+      asyncValue: val
+    })
+
+
   }
 
   render() {
     const { header } = this.props
-    console.log('ssssss')
+
     return (
       <div className="main">
         <OstHeader
@@ -29,6 +58,66 @@ class Home extends Component {
           back={header.back}
           option={header.option}>
         </OstHeader>
+        <div className="main-info">
+          <div className="col1">参保人信息</div>
+          <div className="col2"><span className="left">王小明</span><span className="right">男&nbsp;&nbsp;18岁</span></div>
+          <div className="col3"><span className="left">身份证号</span><span className="right">370**** **** **** *27</span></div>
+        </div>
+        <div className="main-form">
+          <List style={{ backgroundColor: 'white' }} className="picker-list">
+
+            <Picker
+              data={this.state.data}
+              cols={this.state.cols}
+              value={this.state.asyncValue}
+              onPickerChange={this.onPickerChange}
+            >
+              <List.Item arrow="down" onClick={()=>{}}>所在地区</List.Item>
+            </Picker>
+
+            <Picker
+              data={this.state.data}
+              cols={this.state.cols}
+              value={this.state.asyncValue}
+              onPickerChange={this.onPickerChange}
+            >
+              <List.Item arrow="down" onClick={()=>{}}>所在街道</List.Item>
+            </Picker>
+
+            <div className="form-item">详细地址</div>
+            <TextareaItem
+              className="form-text"
+              rows={3}
+              placeholder="请输入"
+              maxLength={100}
+              onChange={(val) => {console.log(val);}}
+           />
+
+           <InputItem
+            className="form-input"
+            type="phone"
+            maxLength={11}
+            placeholder="请输入"
+            >联系电话</InputItem>
+
+            <div className="form-check">
+              <div className="form-check-name">申报类型</div>
+              <div className="form-check-box"><span id={0}>个人</span><span className={'active'} id={0}>单位</span></div>
+            </div>
+
+            <div className="form-item">单位名称</div>
+            <TextareaItem
+              className="form-text"
+              rows={3}
+              placeholder="请输入"
+              onChange={(val) => {console.log(val);}}
+           />
+          </List>
+        </div>
+
+        <div className="main-btn">
+          <Button className="btn">下一步</Button>
+        </div>
       </div>
     )
   }
